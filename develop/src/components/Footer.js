1f154ab76848f7d9ba8c4e4b03b38328c536f569
Carol <carolinemaee@live.com>
2022-10-30 15:18:04 +1030

    footer updated to include links and icons

--- develop/src/components/Footer.js
+++ develop/src/components/Footer.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import '../styles/Footer.css';
+import GitHub from '../images/icon-github.png';
+import LinkedIn from '../images/icon-linkedin.png';
+import StackOverflow from '../images/icon-stackoverflow.png';
 
 function Footer() {
     return (
         <div className='footer'>
-            Created & Designed by Caroline Thomson
+            <div>
+                Created & Designed by <strong>Caroline Thomson</strong>
+            </div>
+            <div className='footer-content'>
+                Find me on: 
+                <a href='https://github.com/carolinemae' target='_blank' rel="noopener noreferrer">
+                    <img src={GitHub} alt='github' className='social-icon'></img>
+                </a>
+                <a href='https://www.linkedin.com/in/caroline-thomson-4279aa23a/' target='_blank' rel="noopener noreferrer">
+                    <img src={LinkedIn} alt='linkedin' className='social-icon'></img>
+                </a>
+                <a href='https://stackoverflow.com/users/19124967/caroline-thomson' target='_blank' rel="noopener noreferrer">
+                    <img src={StackOverflow} alt='stackoverflow' className='social-icon'></img>
+                </a>
+            </div>
         </div>
     )
 }

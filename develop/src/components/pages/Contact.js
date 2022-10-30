ce3c0e5f779da59bbbc6f9d237a9451e695feee7
Carol <carolinemaee@live.com>
2022-10-30 14:47:22 +1030

    phone removed

--- develop/src/components/pages/Contact.js
+++ develop/src/components/pages/Contact.js
@@ -9,19 +9,17 @@ export default function Contact() {
         <div className='form-labels'>
           <p><label for='fname'>First name:</label></p>
           <p><label for='lname'>Last name:</label></p>
-          <p><label for='phone'>Phone:</label></p>
           <p><label for='email'>Email:</label></p>
           <p><label for='message'>Message:</label></p>
         </div>
         <div className='form-input'>
           <p><input type='text' id='fname' name='fname'></input></p>
           <p><input type='text' id='lname' name='lname'></input></p>
-          <p><input type='text' id='phone' name='phone'></input></p>
           <p><input type='text' id='email' name='email'></input></p>
           <p><textarea type='text' id='message' name='message'></textarea></p>
         </div>
       </form>
-      <button>Send!</button>
+      <button>Send</button>
     </div>
   );
 }

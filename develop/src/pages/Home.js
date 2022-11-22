11ff6f87674e8eae1599c00c3c8ef79f17794d2c
Carol <carolinemaee@live.com>
2022-11-22 23:40:40 +1030

    changed name from img to txt

--- develop/src/pages/Home.js
+++ develop/src/pages/Home.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import nameGif from '../images/name-script.gif'
+// import nameGif from '../images/name-script.gif'
 
 export default function Home() {
 
     return (
-        <div id='home'>
-            <div className='anchor content-body home'>
+        <div className='home' id='home'>
+            <div className='anchor content-body home-content'>
                 <h1 className='name'>
-                    {/* Caroline Thomson */}
-                    <img src={nameGif} className='name-img' alt='name' />
+                    Caroline Thomson
+                    {/* <img src={nameGif} className='name-img' alt='name' /> */}
                 </h1>
             </div>
         </div>

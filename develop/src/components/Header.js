0a68dba10663c87e79f263193b648f9eed826f89
Carol <carolinemaee@live.com>
2022-11-22 23:37:53 +1030

    change navbar bg on scroll

--- develop/src/components/Header.js
+++ develop/src/components/Header.js
@@ -5,8 +5,21 @@ import Navbar from 'react-bootstrap/Navbar';
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
 function Header({ currentPage, handlePageChange }) {
+
+  // console.log(document.documentElement.clientHeight);
+
+  window.addEventListener("scroll", (event) => {
+    const navBar = document.querySelector('.navbar');
+    const scroll = window.pageYOffset;
+    if (scroll > 810) {
+      navBar.style.backgroundColor = '#00000060';
+    } else {
+      navBar.style.backgroundColor = '#00000000';
+    }
+  });
+
   return (
-    <Navbar collapseOnSelect expand="lg" className='custom-nav' bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="lg" className='custom-nav' variant="dark">
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav" className='center'>
         <Nav className="me-auto align-center">

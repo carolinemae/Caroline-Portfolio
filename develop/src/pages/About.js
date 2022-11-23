1bcccd88fd1224bf08ab0d76088ccbde10b7a60f
Carol <carolinemaee@live.com>
2022-11-23 12:14:46 +1030

    about content updated

--- develop/src/pages/About.js
+++ develop/src/pages/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import avatar from '../images/avatar-04.jpg';
+import avatar from '../images/avatar-05.jpg';
 
 export default function About() {
   return (
@@ -9,9 +9,22 @@ export default function About() {
           <img src={avatar} className='avatar' alt='avatar' />
         </div>
         <div className='about-content'>
-          Hi! My name is Caroline Thomson, I am a 28 year old living in Adelaide, South Australia.<br/>
-          Recently graduating from the Full Stack Web Developer Boot Camp at the University of Adelaide,<br/>
-          I am hoping to gain employment as a junior full stack developer, and expand my skills and knowledge!<br/>
+          <p>
+            Hi! My name is <em>Caroline Thomson</em>. I am a 28 year old living in Adelaide, South Australia.
+          </p>
+          <p>
+            Recently graduating from the <em>Full Stack Web Developer Boot Camp</em> at the <em>University of Adelaide</em>,
+            I am hoping to gain employment as a junior full stack developer, and expand my skills and knowledge.
+          </p>
+          <p>
+            I have had a keen interest in all things coding since I was 12 years old, where I was creating simple HTML & CSS websites for fun.
+            Since then, any time I've had the chance to learn more about coding, I have thoroughly enjoyed it and have absolutely loved the challenge.
+          </p>
+          <p>
+            Majority of my career I have worked in administrative roles.
+            Naturally with excellent computer literacy, I have always taken on the unofficial role of IT support, helping my team whenever needed.
+            This has helped me develop great problem-solving skills, the ability to show initiative, and work effectively within a team.
+          </p>
         </div>
       </div>
     </div>
